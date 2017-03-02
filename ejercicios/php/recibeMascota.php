57ca391deaf7fd0114f00adcf06349a29da76bc1
CARLES\Carles <developercarles@gmail.com>
2017-03-02 12:32:28 +0100

    ejercicio 2 finalizado

--- ejercicios/php/recibeMascota.php
+++ ejercicios/php/recibeMascota.php
@@ -4,13 +4,20 @@ if(isset($_POST)){
     $eda = $_POST['edad'];
     $tipo = $_POST['tipo'];
     $chip = $_POST['chip'];
-    $castrado = $_POST['castrado'];
+    // $castrado = $_POST['castrado'];
+    if(isset($_POST['castrado'])){
+        $cas = 'No castrado';
+    }else{
+        $cas = 'Castrado';
+    }
     echo '<h2>Grácias por su colaboración</h2>
-          <p>El nombre del animal es: '.$nombre.'</p>
-          <p>La edad del animal es: '.$eda.'</p>
-          <p>El tipo de animals es: '.$tipo.'</p>';
+          <p>El nombre del mascota es: '.$nombre.'</p>
+          <p>La edad del mascota es: '.$eda.'</p>
+          <p>El tipo de mascota es: '.$tipo.'</p>
+          <p>El mascota esta: '.$cas.'</p>';
 }else{
     echo"Error en el envio de datos";
 }
+
 
 ?>
